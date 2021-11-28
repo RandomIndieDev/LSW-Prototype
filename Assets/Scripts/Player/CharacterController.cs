8fc96c4cd8cdf97ab65b04e3f46d05225c4d479a
RandomIndieDev <57868919+RandomIndieDev@users.noreply.github.com>
2021-11-28 13:32:29 +0530

    Collision and Movement
    Added player collision and player movement

---
+++ Assets/Scripts/Player/CharacterController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+
+    public Animator _hairAnimator;
+    public Animator _outfitAnimator;
+
+    [Header("Character Settings")]
+    [SerializeField] private float movementSpeed = 2f;
+
+    private Vector2 motionVector;
+    
+    private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+    private static readonly int Vertical = Animator.StringToHash("Vertical");
+    private static readonly int Moving = Animator.StringToHash("Moving");
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+
+        UpdateControllers();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>("Animation/Player_Skin_2");
+        }
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        _rigidbody2D.velocity = motionVector * movementSpeed;
+    }
+
+    private void UpdateControllers()
+    {
+        motionVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        
+        _animator.SetFloat(Horizontal, motionVector.x);
+        _animator.SetFloat(Vertical, motionVector.y);
+
+        _animator.SetBool(Moving, !(Math.Abs(motionVector.x) < 0.01 && Math.Abs(motionVector.y) < 0.01));
+        
+        _hairAnimator.SetFloat(Horizontal, motionVector.x);
+        _hairAnimator.SetFloat(Vertical, motionVector.y);
+
+        _hairAnimator.SetBool(Moving, !(Math.Abs(motionVector.x) < 0.01 && Math.Abs(motionVector.y) < 0.01));
+        
+        _outfitAnimator.SetFloat(Horizontal, motionVector.x);
+        _outfitAnimator.SetFloat(Vertical, motionVector.y);
+
+        _outfitAnimator.SetBool(Moving, !(Math.Abs(motionVector.x) < 0.01 && Math.Abs(motionVector.y) < 0.01));
+    }
+}
